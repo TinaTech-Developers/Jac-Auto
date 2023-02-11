d223486167aa0b4a171897ac028c3c4215b75740
Tinashe <tinashephiri0@gmail.com>
2023-02-11 16:04:21 +0200

    Jac Auto Pro third content new

--- js/main.js
+++ js/main.js
@@ -126,4 +126,14 @@
     });
     
 })(jQuery);
+// side bar
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 
